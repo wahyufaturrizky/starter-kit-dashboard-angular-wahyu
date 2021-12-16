62106a12e70bd5c67412f50511a52afe23a7200b
WAHYU FATUR RIZKI <wahyu.faturrisky@innovez-one.com>
2021-12-16 08:39:45 +0700

    feat: crud function

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ResUserInterce } from '@app/interface/UserInterface';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
+  @Input() dataTable: Array<ResUserInterce> | undefined;
 
-  constructor() { }
+  @Output() onHandleView: EventEmitter<any> = new EventEmitter();
+  @Output() onHandleEdit: EventEmitter<any> = new EventEmitter();
+  @Output() onHandleDelete: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  handleView(data: ResUserInterce): void {
+    this.onHandleView.emit(data);
   }
 
+  handleEdit(data: ResUserInterce): void {
+    this.onHandleEdit.emit(data);
+  }
+
+  handleDelete(data: ResUserInterce): void {
+    this.onHandleDelete.emit(data);
+  }
+
+  ngOnInit(): void {}
 }
--- src/app/interface/ModalInterface.ts
+++ src/app/interface/ModalInterface.ts
@@ -1,5 +1,9 @@
+import { FormInterface, ResUserInterce } from './UserInterface';
+
 export interface ModalInterface {
   isShow?: boolean;
   headerTitle?: string;
   message?: string;
+  dataRow?: ResUserInterce | undefined;
+  listMenu?: Array<FormInterface>;
 }
